9ba9d7019ae913c341b9449c6ffe9789ae7a200a
ariel3259 <arielsantangelo610@gmail.com>
2021-11-18 17:35:16 -0300

    chalenge finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ClientPokeApi
+
+##  Requeriments: 
+    1 - Chromium based navigator.
+    2 - Internet
+
+## How works ? 
+    Clicks the button, then show up six pokemons into the box;
+
---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+btnShow.onclick = () =>{
+
+    const pokeIdArray = [];
+    for(let i = 0; i < 5; i++){
+        pokeIdArray.push(Math.round( Math.random() * (839-1) + 1))
+    }
+    pokeIdArray.forEach(async id => {
+       try{
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            const pokemon = await response.json();
+            let name = pokemon.species.name;
+            let pokeId = pokemon.id;
+            let image = pokemon.sprites.front_default;
+            let habilities = pokemon.abilities.map( element => element.ability.name );
+            mapPokemon(name, pokeId, habilities, image);
+       }catch(err){
+            console.log(err);
+            return;
+        }
+    })
+}
+
+
+const mapPokemon = (name,id,habilities,image) =>{
+    containerPokemons.innerHTML += 
+    `  
+        <div class = "card">
+            <img class = "img" src="${image}" alt="${name}">
+            <div class = "description-pokemon">
+                <h2>Id : ${id}</h2>
+                <h2>Name : ${name}</h2>
+                <h2>Habilities : ${habilities.map(element => element)}</h2>
+            </div>
+        </div>
+    `;
+}
