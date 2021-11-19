91c939bd08714727040b9d3a248acb170c93a496
ariel3259 <arielsantangelo610@gmail.com>
2021-11-19 13:01:12 -0300

    now can shows six pokemons

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 btnShow.onclick = () =>{
     const pokeIdArray = [];
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i <= 5; i++){
         pokeIdArray.push(Math.round( Math.random() * (839-1) + 1))
     }
     pokeIdArray.forEach(async id => {
@@ -12,9 +12,10 @@ btnShow.onclick = () =>{
             let name = pokemon.species.name;
             let pokeId = pokemon.id;
             let image = pokemon.sprites.front_default;
-            let habilities = pokemon.abilities.map( element => element.ability.name );
+            let abilities = pokemon.abilities.map( element =>  element.ability.name);
             let types = pokemon.types.map( element => element.type.name);
-            mapPokemon(name, pokeId, habilities, image, types);
+            //
+            mapPokemon(name, pokeId, abilities, image, types);
        }catch(err){
             console.log(err);
             return;
@@ -23,7 +24,7 @@ btnShow.onclick = () =>{
 }
 
 
-const mapPokemon = (name, id, habilities, image, types) =>{
+const mapPokemon = (name, id, abilities, image, types) =>{
     containerPokemons.innerHTML += 
     `  
         <div class = "card">
@@ -31,9 +32,13 @@ const mapPokemon = (name, id, habilities, image, types) =>{
             <div class = "description-pokemon">
                 <h2>Id : ${id}</h2>
                 <h2>Name : ${name}</h2>
-                <h2>Habilities : ${habilities.map(element => element)}</h2>
-                <h2>Types : ${types.map( element => element)}</h2>
+                <h2>Types : ${types.map( element => element )}</h2>
+                <h2>Habilities: ${abilities.map(element => element)}</h2>
             </div>
         </div>
     `;
+}
+
+const habilitiesDescription = () => { 
+
 }
